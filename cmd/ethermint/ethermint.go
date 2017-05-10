83ad93912d05864aef76fccb877d3506da3c1e37
ramil <ramilexe@gmail.com>
2017-05-10 19:24:02 +0300

    move flags to utils package

--- cmd/ethermint/ethermint.go
+++ cmd/ethermint/ethermint.go
@@ -6,40 +6,39 @@ import (
 
 	"gopkg.in/urfave/cli.v1"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
+	ethUtils "github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/logger/glog"
 
 	"github.com/tendermint/ethermint/app"
 	"github.com/tendermint/ethermint/ethereum"
 	"github.com/tendermint/ethermint/version"
-	//	minerRewardStrategies "github.com/tendermint/ethermint/strategies/miner"
-	//	validatorsStrategy "github.com/tendermint/ethermint/strategies/validators"
 
 	"github.com/tendermint/abci/server"
+	emtUtils "github.com/tendermint/ethermint/cmd/utils"
 	tendermintNode "github.com/tendermint/tendermint/node"
 )
 
 func ethermintCmd(ctx *cli.Context) error {
 	stack := ethereum.MakeSystemNode(clientIdentifier, version.Version, ctx)
-	utils.StartNode(stack)
+	ethUtils.StartNode(stack)
 
 	addr := ctx.GlobalString("addr")
 	abci := ctx.GlobalString("abci")
 
 	//set verbosity level for go-ethereum
 	glog.SetToStderr(true)
-	glog.SetV(ctx.GlobalInt(VerbosityFlag.Name))
+	glog.SetV(ctx.GlobalInt(emtUtils.VerbosityFlag.Name))
 
 	// Fetch the registered service of this type
 	var backend *ethereum.Backend
 	if err := stack.Service(&backend); err != nil {
-		utils.Fatalf("backend service not running: %v", err)
+		ethUtils.Fatalf("backend service not running: %v", err)
 	}
 
 	// In-proc RPC connection so ABCI.Query can be forwarded over the ethereum rpc
 	client, err := stack.Attach()
 	if err != nil {
-		utils.Fatalf("Failed to attach to the inproc geth: %v", err)
+		ethUtils.Fatalf("Failed to attach to the inproc geth: %v", err)
 	}
 
 	// Create the ABCI app
--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1,4 +1,4 @@
-package main
+package utils
 
 import (
 	"gopkg.in/urfave/cli.v1"
