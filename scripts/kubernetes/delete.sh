c9f29dce7fab90603b05f57cc1c26d17473f1d4d
Node <nodar.chkuaselidze@gmail.com>
2017-06-08 18:14:47 +0400

    Add kubernetes starting tools

---
+++ scripts/kubernetes/delete.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR=`dirname ${BASH_SOURCE[0]}`
+
+echo "Deleting..."
+kubectl delete -f $DIR/ethermint.yaml
+
+echo "Deleteing Volume Claims"
+kubectl delete persistentvolumeclaim --selector="app=tm"
