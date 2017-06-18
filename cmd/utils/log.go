2af07976b46547e908760c407993e4b921a5dc27
Node <nodar.chkuaselidze@gmail.com>
2017-06-18 16:27:56 +0400

    Create TmLogger proxy to Etherum logger

--- cmd/utils/log.go
+++ cmd/utils/log.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/log/term"
+	tmlog "github.com/tendermint/tmlibs/log"
 )
 
 var glogger *log.GlogHandler
@@ -29,4 +30,47 @@ func Setup(ctx *cli.Context) error {
 	log.Root().SetHandler(glogger)
 
 	return nil
+}
+
+func GetTMLogger() tmlog.Logger {
+	return NewTMEthereumProxyLogger()
+}
+
+// Interface assertions
+var _ tmlog.Logger = (*tmEthereumProxyLogger)(nil)
+
+func NewTMEthereumProxyLogger() tmlog.Logger {
+	logger := tmEthereumProxyLogger{}
+	return logger
+}
+
+type tmEthereumProxyLogger struct {
+	keyvals []interface{}
+}
+
+func (l tmEthereumProxyLogger) Debug(msg string, ctx ...interface{}) error {
+	ctx = append(l.keyvals, ctx...)
+	log.Debug(msg, ctx...)
+	return nil
+}
+
+func (l tmEthereumProxyLogger) Info(msg string, ctx ...interface{}) error {
+	ctx = append(l.keyvals, ctx...)
+	log.Info(msg, ctx...)
+	return nil
+}
+
+func (l tmEthereumProxyLogger) Error(msg string, ctx ...interface{}) error {
+	ctx = append(l.keyvals, ctx...)
+	log.Error(msg, ctx...)
+	return nil
+}
+
+func (l tmEthereumProxyLogger) With(ctx ...interface{}) tmlog.Logger {
+	logger := &tmEthereumProxyLogger{}
+	logger.keyvals = make([]interface{}, 0, len(l.keyvals)+len(ctx))
+	logger.keyvals = append(logger.keyvals, l.keyvals...)
+	logger.keyvals = append(logger.keyvals, ctx...)
+
+	return logger
 }
