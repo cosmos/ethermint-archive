54277f1ef3c9c8195100ea5a2a8382cdba31ac7e
ramil <ramilexe@gmail.com>
2017-07-29 09:41:36 +0300

    web3 integration test with docker

---
+++ tests/docker/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker build -t ethermint_tester -f ./tests/docker/Dockerfile .
---
+++ tests/integration/truffle/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker build -t ethermint_js_test -f ./tests/integration/truffle/Dockerfile .
