ac5f5a1ea42881df4c4e87bce1509aec2115da76
ramil <ramilexe@gmail.com>
2017-08-19 06:08:49 +0300

    start cluster of tendermint/ethermint

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@
 set -eux
 
 # count of tendermint/ethermint node
-N=1
+N=4
 
 # Docker version and info
 docker version
@@ -34,27 +34,40 @@ echo
 echo "* [$(date +"%T")] create docker network"
 docker network create --driver bridge --subnet 172.58.0.0/16 ethermint_net
 
-echo
-echo "* [$(date +"%T")] run tendermint container"
 
-#TODO add loop.
-TENDERMINT_IP=$($DIR/p2p/ip.sh 1)
-ETHERMINT_IP=$($DIR/p2p/ip.sh 2)
+#get seeds
+SEEDS=$(bash $DIR/p2p/seeds.sh $N)
+if [[ "$SEEDS" != "" ]]; then
+	SEEDS="--p2p.seeds $SEEDS"
+fi
 
-docker run -d \
-    --net=ethermint_net \
-    --ip "$TENDERMINT_IP" \
-    --name tendermint_1 \
-    -v "$DATA_DIR/tendermint_1:/tendermint" \
-    tendermint/tendermint node --proxy_app tcp://$ETHERMINT_IP:46658
+for i in $(seq 1 "$N"); do
+	echo
+	echo "* [$(date +"%T")] run tendermint $i container"
 
-echo
-echo "* [$(date +"%T")] run ethermint container"
-docker run -d \
-    --net=ethermint_net \
-    --ip $ETHERMINT_IP \
-    --name ethermint_1 \
-    ethermint_tester ethermint --datadir=/ethermint/data --rpc --rpcaddr=0.0.0.0 --ws --wsaddr=0.0.0.0 --rpcapi eth,net,web3,personal,admin --tendermint_addr tcp://$TENDERMINT_IP:46657
+	index=$(($i*2))
+	nextIndex=$(($i*2+1))
+
+	TENDERMINT_IP=$($DIR/p2p/ip.sh $index)
+    ETHERMINT_IP=$($DIR/p2p/ip.sh $nextIndex)
+
+    docker run -d \
+        --net=ethermint_net \
+        --ip "$TENDERMINT_IP" \
+        --name tendermint_$i \
+        -v "$DATA_DIR/tendermint_$i:/tendermint" \
+        tendermint/tendermint node --proxy_app tcp://$ETHERMINT_IP:46658 $SEEDS
+
+	echo
+    echo "* [$(date +"%T")] run ethermint $i container"
+    docker run -d \
+        --net=ethermint_net \
+        --ip $ETHERMINT_IP \
+        --name ethermint_$i \
+        ethermint_tester ethermint --datadir=/ethermint/data --rpc --rpcaddr=0.0.0.0 --ws --wsaddr=0.0.0.0 --rpcapi eth,net,web3,personal,admin --tendermint_addr tcp://$TENDERMINT_IP:46657
+
+done
+
 
 #wait for tendermint & ethermint start
 sleep 60
