2c594ea6ba6b42b12714da37908b68bb771b8d92
Adrian Brink <adrian@brink-holdings.com>
2017-09-06 09:37:55 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Ethermint'
 copyright = u'2017, Adrian Brink'
-author = u'Jae Kwon, Ethan Buchmann, Adrian Brink, Emmanuel Odeke, Zach Ramsay'
+author = u'Tendermint'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
