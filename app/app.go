ff186526393ab25302bb3975aeac0714c36eb9ef
urugang <urugang@gmail.com>
2018-03-29 11:19:00 +0800

    fix LastBlockHash when responding to abci.Info

--- app/app.go
+++ app/app.go
@@ -80,7 +80,7 @@ func (app *EthermintApplication) Info() abciTypes.ResponseInfo {
 	blockchain := app.backend.Ethereum().BlockChain()
 	currentBlock := blockchain.CurrentBlock()
 	height := currentBlock.Number()
-	hash := currentBlock.TxHash()
+	hash := blockchain.LastBlockHash()
 
 	app.logger.Debug("Info", "height", height) // nolint: errcheck
 
